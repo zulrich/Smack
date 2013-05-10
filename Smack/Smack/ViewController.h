70a64ef4ea57d117dba4c23b2587a457e8739b80
Zack Ulrich <zulrich@umich.edu>
2013-05-10 15:26:43 -0400

    first stuff

---
+++ Smack/Smack/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Smack
+//
+//  Created by Zack Ulrich on 5/10/13.
+//  Copyright (c) 2013 Zack Ulrich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Smack/Smack/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Smack
+//
+//  Created by Zack Ulrich on 5/10/13.
+//  Copyright (c) 2013 Zack Ulrich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
